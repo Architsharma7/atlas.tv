7a5b46c2ae1746f17b4ef2293dd606516d744350
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-06-20 00:30:44 +0530

    feat: Lens modules base added

---
+++ contracts/src/lens-module/Follow/FollowModule.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {ICollectModule} from "@aave/lens-protocol/contracts/interfaces/ICollectModule.sol";
+
+contract CollectModule is ICollectModule {}
---
+++ contracts/src/lens-module/Reference/ReferenceModule.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {IFollowModule} from "@aave/lens-protocol/contracts/interfaces/IFollowModule.sol";
+
+contract FollowModule is IFollowModule {}

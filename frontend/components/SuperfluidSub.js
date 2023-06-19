5efe14c73bce90548b8ac89b528b445e80406076
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-06-20 01:05:05 +0530

    feat: raiinbowkit with superfluid widget added

---
+++ frontend/components/SuperfluidSub.js
@@ -0,0 +1,36 @@
+import { WagmiConfig } from "wagmi";
+import SuperfluidWidget from "@superfluid-finance/widget";
+import superTokenList from "@superfluid-finance/tokenlist";
+import { wagmiConfig } from "./wagmi";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+export function atlasSubscriptionComponent() {
+  return (
+    <div>
+      <WagmiConfig config={wagmiConfig}>
+        <ConnectButton.Custom>
+          {({ openConnectModal, connectModalOpen }) => {
+            const walletManager = {
+              open: async () => openConnectModal(),
+              isOpen: connectModalOpen,
+            };
+            return (
+              <SuperfluidWidget
+                {...data}
+                tokenList={superTokenList}
+                type="dialog"
+                walletManager={walletManager}
+              >
+                {({ openModal }) => (
+                  <button onClick={() => openModal()}>
+                    Open Superfluid Widget
+                  </button>
+                )}
+              </SuperfluidWidget>
+            );
+          }}
+        </ConnectButton.Custom>
+      </WagmiConfig>
+    </div>
+  );
+}

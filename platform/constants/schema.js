cb5f0ea97a5703548117c7e78507f7bb3882752a
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-06-29 21:21:22 +0530

    feat: Polybase Updated Schema and Cron jobs added

--- platform/constants/schema.js
+++ platform/constants/schema.js
@@ -1,24 +1,30 @@
 // Tasks
-// ->
+// -> Creators -- containing the info of all the creator
+// -> Users --  containing the info of user like subscribed or not
+// -> Metrics -- info of each creator's total views and watch hours
 
 // @public
 // collection CreatorProfile {
-//   id: string;
+//   id: string; // creator Profile ID from Lens
 //   name: string;
+//   channelDescription ?: string;
+//   channelName ?: string;
+//   profileImageUrl ?: string;
+//   emailId ?: string;
+//   creatorId ?: string;
+//   viewMetrics?: ViewMetrics
+
 //   constructor (id: string, name: string) {
 //     this.id = id;
 //     this.name = name;
 //   }
-//   channelDescription ?: string;
-//     channelName ?: string;
-//     profileImageUrl ?: string;
-//     emailId ?: string;
-//     creatorId ?: string;
+
 //   setCreatorAbout(channelDescription: string,
 //     channelName : string,
 //     profileImageUrl: string,
 //     emailId: string,
-//     creatorId: string){
+//     creatorId: string)
+//   {
 //     this.channelDescription =  channelDescription;
 //     this.channelName = channelName;
 //     this.profileImageUrl= profileImageUrl;
@@ -28,18 +34,46 @@
 // }
 
 // @public
+// collection ViewMetrics {
+//   id: string; // creator Profile ID from Lens
+//   totalViews?: number;
+//   totalWatchHours?: number;
+
+//   dayViews?: number;
+//   dayWatchHours?: number;
+
+//   lastUpdateTimestamp?: number;
+
+//   constructor(id: string){
+//     this.id = id;
+//   }
+
+//   updateMetrices(newTotalviews: number , newWatchHours: number, currentTimeStamp: number){
+//     this.dayViews = newTotalviews - this.totalViews;
+//     this.dayWatchHours = newWatchHours -  this.totalWatchHours;
+
+//     this.totalViews =  newTotalviews;
+//     this.totalWatchHours = newWatchHours;
+
+//     this.lastUpdateTimestamp = currentTimeStamp;
+//   }
+// }
+
+// @public
 // collection UserProfile {
 //   id: string;
 //   name: string;
+//   hasSubscribed: boolean;
+//   userName ?: string;
+//   userEmailId ?: string;
+
 //   constructor (id: string, name: string) {
 //     this.id = id;
 //     this.name = name;
 //   }
-//   userName ?: string;
-//   userEmailId ?: string;
-//   setUserAbout(userName : string,
-//   userEmailId : string)
-// {
+
+//   setUserAbout(userName : string,userEmailId : string)
+//   {
 //     this.userName =  userName;
 //     this.userEmailId = userEmailId;
 //   }

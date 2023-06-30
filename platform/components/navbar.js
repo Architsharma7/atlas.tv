8dbf40ca7c2934a8b9af8b9448c2668512c0f35d
Architsharma7 <architsharma7911@gmail.com>
2023-07-01 02:06:10 +0530

    fix: useronboard and superfluidsub

---
+++ platform/components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-screen'>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Navbar

38b0ea7022baecbd032e6f1b2bc356100ed5032e
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-06-30 12:22:44 +0530

    feat: Contracts and web3 functions configured

--- web3-functions-hardhat/web3-functions/Atlas-Router/index.ts
+++ web3-functions-hardhat/web3-functions/Atlas-Router/index.ts
@@ -6,38 +6,32 @@ import { utils } from "ethers";
 import ky from "ky"; // we recommend using ky as axios doesn't support fetch by default
 
 const Atlas_Router_ABI = [];
+const Atlas_Router_Address = "";
 
 Web3Function.onRun(async (context: Web3FunctionContext) => {
   const { userArgs, storage, multiChainProvider } = context;
 
-  const provider = multiChainProvider.default();
-  const adBoardAddress =
-    (userArgs.adBoard as string) ??
-    "0x28a0A1C63E7E8F0DAe5ad633fe232c12b489d5f0";
+  // fetch all the creators first
+  // fetch the views data for these creators
+  // then prepare the arguements
+  // return the canExec and callData
 
-  const lastPost = Number(await storage.get("lastPost")) ?? 0;
-  //   const adBoardInterface = new utils.Interface(AD_BOARD_ABI);
-
-  const nextPostTime = lastPost + 3600; // 1h
-  const timestamp = (await provider.getBlock("latest")).timestamp;
-
-  if (timestamp < nextPostTime) {
-    return { canExec: false, message: `Time not elapsed` };
-  }
+  const AtlasRouterInterface = new utils.Interface(Atlas_Router_ABI);
 
   try {
   } catch (err) {
     return { canExec: false, message: `QuoteApi call failed` };
   }
 
-  await storage.set("lastPost", timestamp.toString());
-
   return {
     canExec: true,
     callData: [
       {
-        to: adBoardAddress,
-        data: adBoardInterface.encodeFunctionData("postMessage", [message]),
+        to: Atlas_Router_Address,
+        data: AtlasRouterInterface.encodeFunctionData(
+          "distributeSubscriptions",
+          [creators, shares]
+        ),
       },
     ],
   };
